fbea97deebd09cb623da2fdad6cd3ea4fec36b04
alamm28 <alamm28@uni.coventry.ac.uk>
2019-04-15 23:33:04 +0100

    pins

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var express = require('express'),
     crypto = require('crypto'),
     async = require('async'),
     tank = {},
-    _leftMotorFront  = 11,
-    _leftMotorBack   = 12,
-    _rightMotorFront = 15,
-    _rightMotorBack  = 16,
+    p7  = 7,
+    p11   = 11,
+    p13 = 13,
+    p15  = 15,
     app = module.exports = express.createServer(),
     io = sio.listen(app);
 
@@ -42,41 +42,41 @@ console.log('Listening %d in %s mode', app.address().port, app.settings.env);
 
 tank.initPins = function(){
   async.parallel([
-    gpio.open(_leftMotorFront),
-    gpio.open(_leftMotorBack),
-    gpio.open(_rightMotorFront),
-    gpio.open(_rightMotorBack)
+    gpio.setup(p7,gpio.DIR_OUT),
+    gpio.setup(p11,gpio.DIR_OUT),
+    gpio.setup(p13,gpio.DIR_OUT),
+    gpio.setup(p15,gpio.DIR_OUT)
   ]);
 };
 
 tank.moveForward = function(){
   async.parallel([
-    gpio.write(_leftMotorFront, 1),
-    gpio.write(_rightMotorFront, 1)
+    gpio.write(p11, 1),
+    gpio.write(p13, 1)
   ]);
 };
 
 tank.moveBackward = function(){
   async.parallel([
-    gpio.write(_leftMotorBack, 1),
-    gpio.write(_rightMotorBack, 1)
+    gpio.write(p7, 1),
+    gpio.write(p15, 1)
   ]);
 };
 
 tank.turnLeft = function(){
-  gpio.write(_rightMotorFront, 1);
+  gpio.write(p11, 1);
 };
 
 tank.turnRight = function(){
-  gpio.write(_leftMotorFront, 1);
+  gpio.write(p7, 1);
 };
 
 tank.stopAllMotors = function(){
   async.parallel([
-    gpio.write(_leftMotorFront, 0),
-    gpio.write(_leftMotorBack, 0),
-    gpio.write(_rightMotorFront, 0),
-    gpio.write(_rightMotorBack, 0)
+    gpio.write(p11, 0),
+    gpio.write(p13, 0),
+    gpio.write(p15, 0),
+    gpio.write(p7, 0)
   ]);
 };
 
