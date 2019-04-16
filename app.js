7be55d3fc2bf7afc292487e570f2ea965e874919
alamm28 <alamm28@uni.coventry.ac.uk>
2019-04-16 12:43:24 +0100

    added up downand fixed and left right and color and dissss

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ io.sockets.on('connection', function(socket) {
   });
 
 });
-socket.on('disconnect', function(){
+io.socket.on('disconnect', function(){
   console.log("connection lost");
   });
 tank.initPins();
