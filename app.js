863cffe553940f11478eecda0d014203e840a326
alamm28 <alamm28@uni.coventry.ac.uk>
2019-05-01 14:37:58 +0100

    auto

--- app.js
+++ app.js
@@ -172,6 +172,7 @@ io.sockets.on('connection', function (socket) {
     switch (dir) {
       case 'up':
         tank.moveForward();
+        usonic.init();
         var sensor = usonic.createSensor(echo, trig, 10);
         console.log("distance: " + sensor());
         break;
