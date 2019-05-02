447b1783b13a80435045b9568f486eb187061b0c
alamm28 <alamm28@uni.coventry.ac.uk>
2019-05-02 12:32:42 +0100

    auto

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
-const Gpio = require('pigpio').Gpio;
+var Gpio = require('pigpio').Gpio;
 
 // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-const MICROSECDONDS_PER_CM = 1e6/34321;
+var MICROSECDONDS_PER_CM = 1e6/34321;
 
-const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
-const echo = new Gpio(18, {mode: Gpio.INPUT, alert: true});
+var trigger = new Gpio(23, {mode: Gpio.OUTPUT});
+var echo = new Gpio(18, {mode: Gpio.INPUT, alert: true});
 
 trigger.digitalWrite(0); // Make sure trigger is low
 
-const watchHCSR04 = () => {
-  let startTick;
+var watchHCSR04 = () => {
+  var startTick;
 
   echo.on('alert', (level, tick) => {
     if (level == 1) {
       startTick = tick;
     } else {
-      const endTick = tick;
-      const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+      var endTick = tick;
+      var diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
       console.log(diff / 2 / MICROSECDONDS_PER_CM);
     }
   });
