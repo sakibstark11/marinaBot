aa84e4b9878442bb9653bd80db74112694bef2ee
alamm28 <alamm28@uni.coventry.ac.uk>
2019-05-02 16:56:20 +0100

    auto

--- app.js
+++ app.js
@@ -128,8 +128,8 @@ tank.turnRight = function () {
 tank.turnLeft = function () {
   console.log("LEFT");
   async.parallel([
-    gpio.write(p7, 0),
-    gpio.write(p11, 1),
+    gpio.write(p7, 1),
+    gpio.write(p11, 0),
     gpio.write(p13, 0),
     gpio.write(p15, 1)
   ]);
