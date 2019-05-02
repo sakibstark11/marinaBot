0d54deb17f208a3293c7d42e05315597e34933de
alamm28 <alamm28@uni.coventry.ac.uk>
2019-05-02 13:02:37 +0100

    auto

--- app.js
+++ app.js
@@ -65,31 +65,29 @@ tank.moveForward = function () {
   ]);
 };
 var watchHCSR04 = function () {
+  
+};
+tank.getDistance = function () {
   var MICROSECDONDS_PER_CM = 1e6 / 34321;
   var trigger = new Gpio(23, { mode: Gpio.OUTPUT });
   var echo = new Gpio(18, { mode: Gpio.INPUT, alert: true });
   trigger.digitalWrite(0); // Make sure trigger is low
   var startTick;
   var prox;
-  trigger.trigger(10, 1);
+  trigger.trigger(100, 1);
   echo.on('alert', (level, tick) => {
       if (level == 1) {
           startTick = tick;
       } else {
           var endTick = tick;
           var diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-          console.log("difference watch ");
-          console.log(diff / 2 / MICROSECDONDS_PER_CM);
+          console.log("distance ");
           prox = diff / 2 / MICROSECDONDS_PER_CM;
-          distance = prox;
       }
+      console.log(prox);
   });
   trigger.digitalWrite(0);
   echo.digitalWrite(0);
-};
-tank.getDistance = function () {
-  watchHCSR04();
-  console.log(distance);
 };
 
 tank.goup = function () {
