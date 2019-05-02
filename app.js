bc2b05dc1bf4ae6e230164784637dfd853b0b8d1
alamm28 <alamm28@uni.coventry.ac.uk>
2019-05-02 16:49:00 +0100

    auto

--- app.js
+++ app.js
@@ -128,9 +128,9 @@ tank.turnRight = function () {
 tank.turnLeft = function () {
   console.log("LEFT");
   async.parallel([
-    gpio.write(p7, 0),
+    gpio.write(p7, 1),
     gpio.write(p11, 1),
-    gpio.write(p13, 1),
+    gpio.write(p13, 0),
     gpio.write(p15, 1)
   ]);
   tank.getDistance();
